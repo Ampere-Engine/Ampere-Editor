0b7ae69c234d1610fbfd84c9c6e8d2238e00ab00
Mateusz <mateuszslotwinski16@gmail.com>
2022-01-09 15:30:04 +0100

    .

--- src/utilities/FontsAwesome.js
+++
@@ -1,8 +0,0 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-library.add(fas)
-library.add(fab)
-export default FontAwesomeIcon
-//# sourceMappingURL=FontsAwesome.js.map
--- src/utilities/scroll.js
+++
@@ -1,7 +0,0 @@
-const settings = {
-  duration: 500,
-  offset: -100,
-  scrollTo: 0,
-}
-export default settings
-//# sourceMappingURL=scroll.js.map
